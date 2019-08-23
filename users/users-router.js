90bbd9ab7845c6668bd6948f9ae3307131f14beb
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-22 18:20:16 -0600

    Finished MVP

---
+++ users/users-router.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+// Import data model
+const Users = require("./users-model");
+const restricted = require("../auth/restricted-middleware");
+
+// Write CRUD operations
+router.get("/", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      console.log(users);
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+router.delete("/:id", restricted, (req, res) => {
+  const { id } = req.params;
+  Users.remove(id)
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+// Export router
+module.exports = router;
